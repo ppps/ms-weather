baf7c8c282dbd78c9be79ac0f719e993070684be
Rob Wells <rob@robjwells.com>
2014-10-11 16:03:43 +0100

    weather.py: Parse and format location forecast

--- weather.py
+++ weather.py
@@ -39,7 +39,7 @@ weather_types = {'NA': 'Not available',
                  29: 'Thunder shower',      # day
                  30: 'Thunder'}
 
-metoffice_urls = {
+urls = {
     'base': 'http://datapoint.metoffice.gov.uk/public/data/',
     'forecast': 'val/wxfcs/all/json/{location}',
     'summary': 'txt/wxfcs/regionalforecast/json/{location}',
@@ -60,3 +60,68 @@ location_tuples = (
     ('Sheffield', 353467),
     ('Southampton', 353595),
     )
+
+location_dicts = [{'name': a, 'code': b} for a, b in location_tuples]
+forecast_time = (date.today() + timedelta(1)).isoformat() + 'T00:00:00Z'
+
+forecast_codes = {
+    'FDm': {'name': 'Feels like',
+            'units': '°C'},
+    'Dm': {'name': 'Max temp',
+           'units': '°C'},
+    'Gn': {'name': 'Wind gusts',
+           'units': 'mph'},
+    'Hn': {'name': 'Humidity',
+           'units': '%'},
+    'V': {'name': 'Visibility',
+          'units': ''},
+    'D': {'name': 'Wind direction',
+          'units': ''},
+    'S': {'name': 'Wind speed',
+          'units': 'mph'},
+    'U': {'name': 'UV',
+          'units': ''},
+    'W': {'name': 'Weather type',
+          'units': ''},
+    'PPd': {'name': 'Precipitation probability',
+            'units': '%'},
+}
+
+
+response = requests.get(
+    urls['base'] + urls['forecast'].format(location=location_dicts[0]['code']),
+    params={'key': api_key, 'res': 'daily', 'time': forecast_time}
+    )
+conditions = response.json()['SiteRep']['DV']['Location']['Period']['Rep']
+day_forecast = next(f for f in conditions if f['$'] == 'Day')
+
+working_dict = {}
+for k, v in day_forecast.items():
+    if k == '$':
+        continue
+    name = forecast_codes[k]['name']
+    units = forecast_codes[k]['units']
+    if name == 'Weather type':
+        working_dict[name] = weather_types[int(v)]
+    else:
+        working_dict[name] = v + units
+
+weather_template = '''\
+{Weather type}
+{temp}
+Wind {Wind speed} {Wind direction}{precip}'''
+
+temp_string = working_dict['Max temp'] + ' max'
+if working_dict['Feels like'] != working_dict['Max temp']:
+    temp_string += ', feels like {}'.format(working_dict['Feels like'])
+
+precip_chance = working_dict['Precipitation probability']
+if int(precip_chance.replace('%', '')) < 20:
+    precip_string = ''
+else:
+    precip_string = '\n{} chance of rain'.format(precip_chance)
+
+weather_string = weather_template.format(temp=temp_string,
+                                         precip=precip_string,
+                                         **working_dict)
+print(weather_string, '\n')
